aab6627fdb5b868777980e5ef5bd5ca170dc1e27
Jerren <JerrenT@Trifall.com>
2023-04-17 15:31:53 -0400

    Update with flight info

--- app/src/main/java/com/ncorti/kotlin/template/app/MainActivity.kt
+++ app/src/main/java/com/ncorti/kotlin/template/app/MainActivity.kt
@@ -1,43 +1,20 @@
-package com.ncorti.kotlin.template.app
-
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.ncorti.kotlin.template.app.databinding.ActivityMainBinding
-import com.ncorti.kotlin.template.library.FactorialCalculator
-import com.ncorti.kotlin.template.library.android.ToastUtil
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        binding.buttonCompute.setOnClickListener {
-            val message = if (binding.editTextFactorial.text.isNotEmpty()) {
-                val input = binding.editTextFactorial.text.toString().toLong()
-                val result = try {
-                    FactorialCalculator.computeFactorial(input).toString()
-                } catch (ex: IllegalStateException) {
-                    "Error: ${ex.message}"
-                }
+        // Set flight information for departure card
+        departureFlightNumber.text = "Flight Number: ABC123"
+        departureAirportCode.text = "Departure Airport: XYZ"
+        departureTime.text = "Departure Time: 9:00 AM"
 
-                binding.textResult.text = result
-                binding.textResult.visibility = View.VISIBLE
-                getString(R.string.notification_title, result)
-            } else {
-                getString(R.string.please_enter_a_number)
-            }
-            ToastUtil.showToast(this, message)
-        }
-
-        binding.buttonAppcompose.setOnClickListener {
-            val intent = Intent(it.context, ComposeActivity::class.java)
-            startActivity(intent)
-        }
+        // Set flight information for arrival card
+        arrivalAirportCode.text = "Arrival Airport: XYZ"
+        arrivalTime.text = "Arrival Time: 11:00 AM"
     }
 }
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,8 @@ repositories {
 dependencies {
     implementation(libs.kgp)
     implementation(libs.agp)
+    implementation(libs.kotlin)
+    implementation 'net.skyscanner.backpack:backpack-android:43.0.0'
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
